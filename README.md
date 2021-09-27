f5e50af015476dd996c44e1d6a4d47ec46fa8b2c
JoachimByrnesShay <me@joachimbyrnesshay.com>
2021-09-27 01:34:25 -0400

    media query added; some temporary styling added; short event listeners added to javascript onclick function arg to ensure smooth operation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### data visualization project, ver 1
 ### 9/27/21
+### currency conversation data, 
+### credit to https://api.exchangerate.host/' for data including symbols and conversion rates
+### barchart
---
+++ js/main.js
@@ -0,0 +1,26 @@
+console.log('main.js successfully loads')
+
+
+function thisModal(elem) {
+    console.log(elem);
+    const startTime = Date.now();
+
+
+    let modal = elem.querySelector('.modal');
+    // modal.innerHTML = 'Im a modal buddy';
+    // // modal.style.display = 'block';
+    // // elem.addEventListener("mouseleave", () => modal.style.display = 'none');
+    modal.classList.add('showModal')
+    modal.addEventListener("animationend", () => {
+        // element.style['display'] = 'none';
+        modal.classList.remove('showModal')
+    })
+    modal.addEventListener('animationcancel', (e) => {
+
+        modal.classList.remove('showModal')
+
+
+    })
+
+
+}
