6d6eda96a2688ef8442a742cde92262f78ae91a8
JoachimByrnesShay <me@joachimbyrnesshay.com>
2021-09-27 17:18:08 -0400

    added bulgarian lev currency example

--- js/main.js
+++ js/main.js
@@ -1,34 +1,13 @@
-console.log('main.js successfully loads')
-
+console.log('main.js successfully loads');
 
 function thisModal(elem) {
-    console.log(elem);
-    const startTime = Date.now();
-
-
+    console.log('thisModal function called');
     let modal = elem.querySelector('.modal');
-    // modal.innerHTML = 'Im a modal buddy';
-    // // modal.style.display = 'block';
-    // // elem.addEventListener("mouseleave", () => modal.style.display = 'none');
-    modal.classList.add('showModal')
+    modal.classList.add('showModal');
     modal.addEventListener("animationend", () => {
-        // element.style['display'] = 'none';
-        modal.classList.remove('showModal')
-    })
-    modal.addEventListener('animationcancel', (e) => {
-
-        modal.classList.remove('showModal')
+        modal.classList.remove('showModal');
     });
-    // modal.setAnimationListener(null);
-    // // })
-    modal.addAnimationListener('animationstart', (e) => {
-        e.preventDefault();
-
-        e.stopImmediatePropagation();
+    modal.addEventListener('animationcancel', () => {
+        modal.classList.remove('showModal');
     });
-
 }
-
-// window.onresize = () => {
-        //     alert('hi')
-        // }
