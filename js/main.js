3406287ec97644e248013347eba61410faba00fb
JoachimByrnesShay <me@joachimbyrnesshay.com>
2021-09-28 03:40:35 -0400

    added comments, tweaked a few css variables

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
-/** show modal, call with onClick in html **/
+/** show modal, call via onClick in html **/
 function activateModal(elem) {
+    /* elem is the ChartContent-barChart element which has been clicked, select its modal only */
     let modal = elem.querySelector('.Modal');
 
     modal.classList.add('is-displayed');
 
+    /* cleanly remove is-displayed state class from modal when animation ends */
     modal.addEventListener("animationend", () => {
         modal.classList.remove('is-displayed');
     });
-    /** smoothens user experience if user moves cursor back to bar too quickly**/
+    /** smoothens user experience if user moves cursor back to bar too quickly, without this- after modal animation starts, 
+     * if user moves cursor back to chart, modal will pop up again because of this class remaining on the element*/
     modal.addEventListener('animationcancel', () => {
         modal.classList.remove('is-displayed');
     });
